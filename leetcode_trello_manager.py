de1771c587001a01c6b63dc4e878157ea03b0335
grannyprogramming <amcg809@gmail.com>
2023-09-15 20:52:14 +0100

    feat(leetcode_trello_manager.py): update board background logic
    Update board background logic to use the correct endpoint and entity for the Trello API request.

--- leetcode_trello_manager.py
+++ leetcode_trello_manager.py
@@ -100,9 +100,10 @@ def upload_custom_board_background(config, settings, member_id, image_filepath):
     return response.get('id') if response else None
 
 def set_custom_board_background(config, settings, board_id, background_id):
-    endpoint = f"boards/{board_id}/prefs/background"
-    response = trello_request(config, settings, endpoint, method="PUT", value=background_id)
+    endpoint = f"{board_id}/prefs/background"
+    response = trello_request(config, settings, endpoint, method="PUT", entity="boards", value=background_id)
     return response if response else None
+
 
 
 def set_board_background(config, settings, board_id):
