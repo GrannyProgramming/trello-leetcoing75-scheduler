a5720ac5d0cbb80718631b7b926a712113d85824
grannyprogramming <amcg809@gmail.com>
2023-09-15 20:01:11 +0100

    Commit: feat(leetcode_trello_manager): refactor make_request and trello_request
    Refactor make_request and trello_request functions to remove unnecessary parameters and improve readability.

--- leetcode_trello_manager.py
+++ leetcode_trello_manager.py
@@ -33,19 +33,19 @@ def load_config():
     }
 
 
-def make_request(url, method, params, headers=None):
-    with requests.request(method, url, params=params, headers=headers) as response:
+def make_request(url, method, params):
+    with requests.request(method, url, params=params) as response:
         if response.status_code != 200:
             logging.error(f"Request to {url} failed with status code {response.status_code}. Response: {response.text}")
             return None
         return response.json()
 
-def trello_request(config, settings, resource, method="GET", entity="boards", headers=None, **kwargs):
+def trello_request(config, settings, resource, method="GET", entity="boards", **kwargs):
     logging.info(f"Making a request to endpoint: {entity}/{resource}")
     url = f"{settings['BASE_URL']}/{entity}/{resource}"
     query = {'key': config['API_KEY'], 'token': config['OAUTH_TOKEN'], **kwargs}
     
-    return make_request(url, method, query, headers)
+    return make_request(url, method, query)
 
 
 def get_board_id(config, settings, name):
