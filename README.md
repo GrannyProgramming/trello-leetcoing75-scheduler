88923425e9af203070c64027586cec735a09afd0
grannyprogramming <amcg809@gmail.com>
2023-09-15 13:55:16 +0100

    chore(.github/workflows/run_trello_script.yml): Add workflow for running Trello script
    Add a GitHub workflow for running the Trello script, update the license and readme, add an image for the API key, and add the Tre

--- README.md
+++ README.md
@@ -1,2 +1,96 @@
-# trello-leetcoing75-scheduler
-Automates the process of scheduling and managing LeetCode challenges on a Trello board. This repository contains a script that organizes coding challenges by topic, difficulty, and sets due dates for each problem. Designed to streamline the study plan for the "75 LeetCode Questions" approach to mastering coding interviews.
+# Trello LeetCoding75 Scheduler 💡
+
+Automate and optimize your LeetCode challenge preparation with the Trello LeetCoding75 Scheduler. This tool helps create a systematic study plan by categorizing challenges on a Trello board based on topic, difficulty, and due dates.
+
+## Features 🌟
+
+- **Trello Integration**: Direct interaction with the Trello API for seamless card, list, and label management.
+- **Topic-based Categorization**: Challenges are grouped under topics like Arrays, Two Pointers, Binary Trees, etc., for a comprehensive study approach.
+- **Automated Due Dates**: Assigns due dates to challenges, offering a structured study plan.
+- **Direct Challenge Links**: Each card contains a direct link to the respective LeetCode challenge for easy access.
+- **Customizability**: Modify the script to adapt to individual study plans or different challenge sets.
+
+## Spaced Repetition Learning 🧠🔄
+
+One of the core principles that influenced the design of this scheduler is the concept of **Spaced Repetition**. It's a scientifically-backed learning technique where information is reviewed at increasing intervals, ensuring long-term retention and efficient learning. By spacing out your LeetCode challenges, you're not just practicing but also reinforcing your memory and understanding of algorithms and data structures.
+
+### Why Spaced Repetition?
+
+- **Enhanced Retention**: It reduces the forgetting curve, ensuring that you remember algorithms and solutions for a longer duration.
+- **Efficient Learning**: Instead of cramming, spaced repetition promotes steady, incremental learning, which is proven to be more effective.
+- **Adaptive**: The technique adjusts to your learning pace. Topics you're familiar with will appear less often, while challenging ones will be reviewed more frequently.
+
+The Trello LeetCoding75 Scheduler inherently supports spaced repetition. By categorizing challenges and automating due dates, you're encouraged to revisit topics and problems periodically, ensuring a deeper understanding and better performance in coding interviews.
+
+## Setup 🛠️
+
+### 1. Obtain API Key from Trello:
+
+- **Login**: Ensure you're logged into your [Trello account](https://trello.com/).
+- **Developer Portal**: Navigate to the [Power-Up Admin Portal](https://trello.com/power-ups/admin/).
+- **API Key**: View and manage your developer API keys. If you haven't created one, add a new one. Once created, copy and safely store your API key.
+
+    ![API Key Creation](./imgs/api_key.png)
+
+### 2. Obtain OAuth Token from Trello:
+
+- **Generate Token**: After obtaining your API key, [click here](https://trello.com/1/authorize?expiration=1day&name=MyPersonalToken&scope=read,write&response_type=token&key=YOUR_API_KEY). Replace `YOUR_API_KEY` with your Trello API key.
+- **Authorize**: Grant the token access to your Trello account.
+- **Get Token**: Post authorization, you'll receive an OAuth token. Copy and store it securely.
+
+### 3. Store Tokens as GitHub Secrets:
+
+- **Repository**: Navigate to your GitHub repository.
+- **Settings**: Access the Settings tab on your repository page.
+- **Secrets**: On the left sidebar, click on Secrets & Variables.
+- **Add Secrets**: Click on the `New repository secret` button. Add both `API_KEY` (from step 1) and `TRELLO_OAUTH_TOKEN` (from step 2).
+
+## Usage 🚀
+
+This repository is set up with a GitHub Actions workflow. Once the setup is complete, the script (`leetcode_trello_manager.py`) will automatically run every Monday at 01:00 UTC, as defined in the workflow.
+
+### Manual Trigger (Optional):
+
+If you wish to trigger the script manually:
+
+1. Navigate to the `Actions` tab of your GitHub repository.
+2. Select the `Run Trello Script` workflow from the left sidebar.
+3. Click on `Run workflow` to manually trigger the script.
+
+Note: Ensure that the secrets (`API_KEY` and `TRELLO_OAUTH_TOKEN`) are set up correctly in the repository for the script to function as expected during the workflow run.
+
+## Troubleshooting 🛠️🔍
+
+During the course of using this scheduler, you might encounter certain issues. Here are some common ones and their solutions:
+
+### GitHub Action Failures:
+
+- **Error Logs**: Always check the error logs provided by GitHub Actions. They can provide insights into what went wrong.
+- **Secrets**: Ensure the `API_KEY` and `TRELLO_OAUTH_TOKEN` are correctly set in your repository secrets.
+- **Trello Permissions**: The provided API key and OAuth token must have the necessary permissions on Trello to create and manage boards, lists, and cards.
+
+### Trello Authentication Issues:
+
+- **Token Expiry**: If you face authentication issues, it might be because the OAuth token has expired. Follow the steps in the setup section to generate a new token.
+- **API Key**: Ensure the API key hasn't been revoked or deleted from the Trello Developer Portal.
+
+If you face an issue that's not listed above, please raise an issue in the GitHub repository, and we'll address it at the earliest.
+
+## Customization 🎨
+
+The Trello LeetCoding75 Scheduler is designed to be flexible and customizable according to your needs. If you'd like to tailor the challenges, scheduling, or any other aspect, here's how you can get started:
+
+1. **Modify Challenges**: The `topics` dictionary in `leetcode_trello_manager.py` contains all the challenges categorized by topics. You can add, remove, or modify challenges as needed.
+2. **Adjust Scheduling**: The function `generate_all_due_dates` determines the due dates for challenges. Modify this if you'd like to change the scheduling logic.
+3. **Trello Board Customization**: The board name, lists, and other configurations are defined within the script. Adjust these constants and functions to customize the Trello board's appearance and structure.
+
+Remember to thoroughly test your changes locally before deploying them to ensure the scheduler's smooth operation.
+
+
+## Contribution 🤝
+
+Feel free to fork this repository, make changes, and open a pull request if you think you can make any improvements or have any suggestions.
+
+## License ⚖️
+
+[MIT License](LICENSE)
---
+++ leetcode_trello_manager.py
@@ -0,0 +1,257 @@
+import requests
+import logging
+from datetime import datetime, timedelta
+import os
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+current_date = datetime.now()
+API_KEY = os.environ['API_KEY']
+OAUTH_TOKEN = os.environ['OAUTH_TOKEN']
+BASE_URL = "https://api.trello.com/1"
+BOARD_NAME = "LeetCode Challenges"
+topics = {
+    "Array / String": [
+        {"title": "Merge Strings Alternately", "difficulty": "Easy"},
+        {"title": "Greatest Common Divisor of Strings", "difficulty": "Easy"},
+        {"title": "Kids With the Greatest Number of Candies", "difficulty": "Easy"},
+        {"title": "Can Place Flowers", "difficulty": "Easy"},
+        {"title": "Reverse Vowels of a String", "difficulty": "Easy"},
+        {"title": "Reverse Words in a String", "difficulty": "Medium"},
+        {"title": "Product of Array Except Self", "difficulty": "Medium"},
+        {"title": "Increasing Triplet Subsequence", "difficulty": "Medium"},
+        {"title": "String Compression", "difficulty": "Medium"},
+    ],
+    "Two Pointers": [
+        {"title": "Move Zeroes", "difficulty": "Easy"},
+        {"title": "Is Subsequence", "difficulty": "Easy"},
+        {"title": "Container With Most Water", "difficulty": "Medium"},
+        {"title": "Max Number of K-Sum Pairs", "difficulty": "Medium"},
+    ],
+    "Sliding Window": [
+        {"title": "Maximum Average Subarray I", "difficulty": "Easy"},
+        {"title": "Maximum Number of Vowels in a Substring of Given Length", "difficulty": "Medium"},
+        {"title": "Max Consecutive Ones III", "difficulty": "Medium"},
+        {"title": "Longest Subarray of 1's After Deleting One Element", "difficulty": "Medium"},
+    ],
+    "Prefix Sum": [
+        {"title": "Find the Highest Altitude", "difficulty": "Easy"},
+        {"title": "Find Pivot Index", "difficulty": "Easy"},
+    ],
+    "Hash Map / Set": [
+        {"title": "Find the Difference of Two Arrays", "difficulty": "Easy"},
+        {"title": "Unique Number of Occurrences", "difficulty": "Easy"},
+        {"title": "Determine if Two Strings Are Close", "difficulty": "Medium"},
+        {"title": "Equal Row and Column Pairs", "difficulty": "Medium"},
+    ],
+    "Stack": [
+        {"title": "Removing Stars From a String", "difficulty": "Medium"},
+        {"title": "Asteroid Collision", "difficulty": "Medium"},
+        {"title": "Decode String", "difficulty": "Medium"},
+    ],
+    "Queue": [
+        {"title": "Number of Recent Calls", "difficulty": "Easy"},
+        {"title": "Dota2 Senate", "difficulty": "Medium"},
+    ],
+    "Linked List": [
+        {"title": "Delete the Middle Node of a Linked List", "difficulty": "Medium"},
+        {"title": "Odd Even Linked List", "difficulty": "Medium"},
+        {"title": "Reverse Linked List", "difficulty": "Easy"},
+        {"title": "Maximum Twin Sum of a Linked List", "difficulty": "Medium"},
+    ],
+    "Binary Tree - DFS": [
+        {"title": "Maximum Depth of Binary Tree", "difficulty": "Easy"},
+        {"title": "Leaf-Similar Trees", "difficulty": "Easy"},
+        {"title": "Count Good Nodes in Binary Tree", "difficulty": "Medium"},
+        {"title": "Path Sum III", "difficulty": "Medium"},
+        {"title": "Longest ZigZag Path in a Binary Tree", "difficulty": "Medium"},
+        {"title": "Lowest Common Ancestor of a Binary Tree", "difficulty": "Medium"},
+    ],
+        "Binary Tree - BFS": [
+        {"title": "Binary Tree Right Side View", "difficulty": "Medium"},
+        {"title": "Maximum Level Sum of a Binary Tree", "difficulty": "Medium"},
+    ],
+    "Binary Search Tree": [
+        {"title": "Search in a Binary Search Tree", "difficulty": "Easy"},
+        {"title": "Delete Node in a BST", "difficulty": "Medium"},
+    ],
+    "Graphs - DFS": [
+        {"title": "Keys and Rooms", "difficulty": "Medium"},
+        {"title": "Number of Provinces", "difficulty": "Medium"},
+        {"title": "Reorder Routes to Make All Paths Lead to the City Zero", "difficulty": "Medium"},
+        {"title": "Evaluate Division", "difficulty": "Medium"},
+    ],
+    "Graphs - BFS": [
+        {"title": "Nearest Exit from Entrance in Maze", "difficulty": "Medium"},
+        {"title": "Rotting Oranges", "difficulty": "Medium"},
+    ],
+    "Heap / Priority Queue": [
+        {"title": "Kth Largest Element in an Array", "difficulty": "Medium"},
+        {"title": "Smallest Number in Infinite Set", "difficulty": "Medium"},
+        {"title": "Maximum Subsequence Score", "difficulty": "Medium"},
+        {"title": "Total Cost to Hire K Workers", "difficulty": "Medium"},
+    ],
+    "Binary Search": [
+        {"title": "Guess Number Higher or Lower", "difficulty": "Easy"},
+        {"title": "Successful Pairs of Spells and Potions", "difficulty": "Medium"},
+        {"title": "Find Peak Element", "difficulty": "Medium"},
+        {"title": "Koko Eating Bananas", "difficulty": "Medium"},
+    ],
+    "Backtracking": [
+        {"title": "Letter Combinations of a Phone Number", "difficulty": "Medium"},
+        {"title": "Combination Sum III", "difficulty": "Medium"},
+    ],
+    "DP - 1D": [
+        {"title": "N-th Tribonacci Number", "difficulty": "Easy"},
+        {"title": "Min Cost Climbing Stairs", "difficulty": "Easy"},
+        {"title": "House Robber", "difficulty": "Medium"},
+        {"title": "Domino and Tromino Tiling", "difficulty": "Medium"},
+    ],
+    "DP - Multidimensional": [
+        {"title": "Unique Paths", "difficulty": "Medium"},
+        {"title": "Longest Common Subsequence", "difficulty": "Medium"},
+        {"title": "Best Time to Buy and Sell Stock with Transaction Fee", "difficulty": "Medium"},
+        {"title": "Edit Distance", "difficulty": "Medium"},
+    ],
+    "Bit Manipulation": [
+        {"title": "Counting Bits", "difficulty": "Easy"},
+        {"title": "Single Number", "difficulty": "Easy"},
+        {"title": "Minimum Flips to Make a OR b Equal to c", "difficulty": "Medium"},
+    ],
+    "Trie": [
+        {"title": "Implement Trie (Prefix Tree)", "difficulty": "Medium"},
+        {"title": "Search Suggestions System", "difficulty": "Medium"},
+    ],
+    "Intervals": [
+        {"title": "Non-overlapping Intervals", "difficulty": "Medium"},
+        {"title": "Minimum Number of Arrows to Burst Balloons", "difficulty": "Medium"},
+    ],
+    "Monotonic Stack": [
+        {"title": "Daily Temperatures", "difficulty": "Medium"},
+        {"title": "Online Stock Span", "difficulty": "Medium"},
+    ],
+}
+
+def request_trello(endpoint, method="GET", **kwargs):
+    logging.info(f"Making a request to endpoint: {endpoint}")
+    url = f"{BASE_URL}{endpoint}"
+    query = {'key': API_KEY, 'token': OAUTH_TOKEN, **kwargs}
+    response = requests.request(method, url, params=query)
+    return response.json()
+
+def get_board_id(name):
+    return next((board['id'] for board in request_trello("/members/me/boards", filter="open") if board['name'] == name), None)
+
+def generate_leetcode_link(title):
+    return f"https://leetcode.com/problems/{title.lower().replace(' ', '-')}/"
+
+def compute_due_date(start_date, days):
+    due_date = start_date
+    while days > 0:
+        due_date += timedelta(days=1)
+        if due_date.weekday() < 5:
+            days -= 1
+    return due_date
+
+def is_due_this_week(due_date):
+    start_of_week = current_date - timedelta(days=current_date.weekday())
+    end_of_week = start_of_week + timedelta(days=4)
+    return start_of_week <= due_date <= end_of_week
+
+def get_next_working_day(date):
+    next_day = date + timedelta(days=1)
+    while next_day.weekday() >= 5:
+        next_day += timedelta(days=1)
+    return next_day
+
+def card_exists(board_id, card_name):
+    """Check if a card with the given name exists on the board."""
+    cards = request_trello(f"/boards/{board_id}/cards")
+    return any(card['name'] == card_name for card in cards)
+
+def generate_all_due_dates(topics, start_date):
+    due_dates = []
+    next_due_date = get_next_working_day(start_date)
+    for topic, problems in topics.items():
+        for _ in problems:
+            due_dates.append(next_due_date)
+            next_due_date = get_next_working_day(next_due_date)
+    return due_dates
+
+def retest_cards(board_name):
+    # Retrieve all cards from the Retrospective and Completed lists
+    board_id = get_board_id(board_name)
+    list_ids = {l['name']: l['id'] for l in request_trello(f"/boards/{board_id}/lists")}
+    retrospective_cards = request_trello(f"/lists/{list_ids['Retrospective']}/cards")
+    completed_cards = request_trello(f"/lists/{list_ids['Completed']}/cards")
+
+    # Process cards from the Retrospective list
+    for card in retrospective_cards:
+        label_names = [label['name'] for label in card['labels']]
+        if "Do not know" in label_names:
+            new_due_date = get_next_working_day(current_date)
+            list_name = "Do this week"
+        elif "Somewhat know" in label_names:
+            new_due_date = get_next_working_day(current_date + timedelta(weeks=1))
+            list_name = "Do this week" if is_due_this_week(new_due_date) else "Backlog"
+        elif "Know" in label_names:
+            new_due_date = get_next_working_day(current_date + timedelta(weeks=4))
+            list_name = "Completed"
+        else:
+            continue  # If the card doesn't have any of the expected labels, skip it
+
+        # Move card to the correct list based on its recalculated due date
+        request_trello(f"/cards/{card['id']}", "PUT", idList=list_ids[list_name], due=new_due_date.isoformat())
+
+    # Process cards from the Completed list
+    for card in completed_cards:
+        current_due_date = datetime.fromisoformat(card['due'].replace('Z', ''))
+        if is_due_this_week(current_due_date):
+            # Move card to the "Do this week" list without changing its due date
+            request_trello(f"/cards/{card['id']}", "PUT", idList=list_ids["Do this week"])
+
+    logging.info("Retest cards processed!")
+
+def setup_board(board_name, topics):
+    board_id = get_board_id(board_name) or request_trello("/boards", "POST", name=board_name)['id']
+    existing_list_names = {lst['name'] for lst in request_trello(f"/boards/{board_id}/lists", cards="none")}
+    
+    # Delete default lists
+    for default_list in ["To Do", "Doing", "Done"]:
+        if default_list in existing_list_names:
+            list_id = next(lst['id'] for lst in request_trello(f"/boards/{board_id}/lists", cards="none") if lst['name'] == default_list)
+            request_trello(f"/lists/{list_id}/closed", "PUT", value='true')
+
+    # Create required lists
+    for required_list in ["Completed", "Retrospective", "Do this week", "Backlog"]:
+        if required_list not in existing_list_names:
+            response = request_trello("/lists", "POST", idBoard=board_id, name=required_list)
+
+    list_ids = {l['name']: l['id'] for l in request_trello(f"/boards/{board_id}/lists")}
+    label_colors = {'Easy': 'green', 'Medium': 'yellow', 'Somewhat know': 'blue', 'Do not know': 'red', 'Know': 'green'}
+    
+    for label, color in label_colors.items():
+        labels = request_trello(f"/boards/{board_id}/labels")
+        if label not in [l['name'] for l in labels]:
+            request_trello("/labels", "POST", name=label, color=color, idBoard=board_id)
+    
+    label_ids = {l['name']: l['id'] for l in request_trello(f"/boards/{board_id}/labels")}
+    all_due_dates = generate_all_due_dates(topics, current_date)
+    due_date_index = 0  # Initialize due_date_index at the beginning of the function
+
+    for category, problems in topics.items():
+        topic_label_id = request_trello("/labels", "POST", name=category, color="black", idBoard=board_id)['id']
+        for problem in problems:
+            card_name = f"{category}: {problem['title']}"
+            if not card_exists(board_id, card_name):
+                difficulty_label_id = label_ids[problem["difficulty"]]
+                link = generate_leetcode_link(problem["title"])                
+                list_name = "Do this week" if is_due_this_week(all_due_dates[due_date_index]) else "Backlog"
+                due_date_for_card = all_due_dates[due_date_index]
+                due_date_index += 1
+                request_trello("/cards", "POST", idList=list_ids[list_name], name=card_name, desc=link, idLabels=[difficulty_label_id, topic_label_id], due=due_date_for_card.isoformat())
+
+    logging.info("Trello board setup completed!")
+
+setup_board(BOARD_NAME, topics)
+retest_cards(BOARD_NAME)
