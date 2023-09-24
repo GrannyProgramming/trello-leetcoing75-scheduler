5db846ea2164b0dadacb1d6a336cb486b1e864e5
grannyprogramming <amcg809@gmail.com>
2023-09-24 18:02:05 +0100

    Commit message: feat(card_operations): update card due date and move completed cards to 'Do this week' list

--- src/modules/card_operations.py
+++ src/modules/card_operations.py
@@ -307,12 +307,13 @@ def process_retrospective_cards(config, settings, board_id, current_date):
             trello_request(
                 config,
                 settings,
-                resource=card['id'],             
-                method="PUT",
+                "cards/" + card['id'],             
+                "PUT",
                 entity="cards",         
                 idList=list_ids[list_name],
                 due=new_due_date.isoformat(),
             )
+
 
 
 
